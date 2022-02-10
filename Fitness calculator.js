173d5636ee2c09d6a7fe0f3dd693083c60e9dbc2
AtampizDany21 <97300841+AtampizDany21@users.noreply.github.com>
2022-02-10 10:06:02 -0300

    Update Fitness calculator.js
    Double checked on the file loaded. I change the .toPrecision attribute to (3) instead of (2) to show a decimal number instead of an integer. Also provided more comments on each step done.

--- Fitness calculator.js
+++ Fitness calculator.js
@@ -1,21 +1,22 @@
 
-// Step 1:This function takes two arguments, weight and height to calculate the bmi of a person.
+// This function takes two arguments, weight and height to calculate the bmi of a person.
 function bmi(weight,height) {
 
-    // Step 2: In the function, return the division of the parameters weight and height elevated by 2
+    // The bmi function returns the user height elevated by 2 and divided by its weight.
     return  weight / (height * height);
 }
     
-    // Using function declaration, define a function named bmiCalculation that gets the values of two HTML form controls with IDs of weight and height. Pass them to the bmi function
+    // Declarate bmiCalculations function, which gets the values of two HTML form controls with IDs of weight and height. Then pass them to the bmi function.
 function bmiCalculations(){
     let weight = parseInt(document.querySelector("#weight").value);
     let height = parseInt(document.querySelector("#height").value)/ 100;
     let result = bmi(weight,height);
     
     
-
+//Declare a variable to hold the message to be printed for each category, according to the bmi function result.
     let message;
 
+    // If, else if, else statements to print only the correct message.
         if (result < "18.5") {
             message = "Underweight";
         }
@@ -36,10 +37,17 @@ function bmiCalculations(){
             message = "Obese class 2";
         }
 
+        // Assign the value of the message variable to the HTML element with an ID of message.
             document.querySelector("#message").textContent = message;
 
-            // Assign the return value to an HTML form element with an ID of total
-    document.querySelector("#total").value = result.toPrecision(2);
+            // Assign the return value to an HTML form element with an ID of total. toPrecision stablishes the number will be set to 3 digits total. 
+            // generally It will show 2 integers and 1 decimal number.
+    document.querySelector("#total").value = result.toPrecision(3);
 }
-// Add a "click" event listener to the HTML button with an ID of bmiCalculations that calls the bmi function
+// Add a "click" event listener to the HTML button with an ID of bmiCalculations that calls the bmiCalculation function.
 document.querySelector("#bmiCalculations").addEventListener("click", bmiCalculations);
+    
+
+
+
+   
